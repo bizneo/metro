f8643f921d3d5a03ed8f492d6356f9678b62175a
Óscar de Arriba <oscar@dearriba.es>
2023-11-23 12:49:12 +0100

    Fix lib start

--- mix.exs
+++ mix.exs
@@ -14,8 +14,7 @@ defmodule Metro.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Metro, []}
+      extra_applications: [:logger]
     ]
   end
 
