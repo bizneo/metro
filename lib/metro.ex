725e63ef0ce396e4ae7a6caf3685bfe68aaa54aa
Óscar de Arriba <oscar@dearriba.es>
2023-11-24 10:15:22 +0100

    Update startup process

--- lib/metro.ex
+++ lib/metro.ex
@@ -10,22 +10,21 @@ defmodule Metro do
   def start(_, opts), do: start_link(opts)
 
   def start_link(opts) do
-    Supervisor.start_link(__MODULE__, opts, name: Metro.Supervisor)
+    with {:ok, pid} <- Supervisor.start_link(__MODULE__, opts, name: Metro.Supervisor) do
+      Logger.info("Running Metro at http://localhost:#{get_port(opts)}/metrics")
+      {:ok, pid}
+    end
   end
 
   @impl Supervisor
   def init(opts) do
-    port = Keyword.get(opts, :port, 9000)
-
     children = [
-      {Plug.Cowboy, scheme: :http, plug: Metro.Plug, options: [port: port]}
+      {Plug.Cowboy, scheme: :http, plug: Metro.Plug, options: [port: get_port(opts)]}
     ]
 
     attach_telemetry!(opts)
     Metro.EctoInstrumenter.setup()
     Metro.Plug.setup()
-
-    Logger.info("Running Metro at http://localhost:9000/metrics")
 
     Supervisor.init(children, strategy: :one_for_one)
   end
@@ -40,5 +39,9 @@ defmodule Metro do
         &Metro.EctoInstrumenter.handle_event/4,
         nil
       )
+  end
+
+  defp get_port(opts) do
+    Keyword.get(opts, :port, 9000)
   end
 end
